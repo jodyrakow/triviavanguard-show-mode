2d3f0ea115d07cace98822f87f41873e9bb82b65
Jody Rakow <jodyrakow@Jodys-New-Computer.local>
2025-07-08 07:25:19 -0500

    Hoping to handle two different JSON formats

--- netlify/functions/fetchShows.js
+++ netlify/functions/fetchShows.js
@@ -5,19 +5,28 @@ const axios = require("axios");
 exports.handler = async function (event, context) {
   const makeWebhookUrl = "https://hook.us2.make.com/ijqo3vhlwtlrsr8sfci1osw8s1j8wtfd";
 
-  try {
-    const response = await axios.get(makeWebhookUrl);
+const res = await axios.get(makeWebhookUrl);
+let data = res.data;
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response.data),
-    };
-  } catch (error) {
-    console.error("Serverless error:", error);
+let formattedData;
 
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Failed to fetch from Make", error: error.message }),
-    };
-  }
+if (Array.isArray(data)) {
+  // Format: [ { Shows: [...] }, { Rounds: [...] } ]
+  formattedData = {
+    Shows: data.find(obj => obj.Shows)?.Shows || [],
+    Rounds: data.find(obj => obj.Rounds)?.Rounds || []
+  };
+} else {
+  // Format: { Shows: [...], Rounds: [...] }
+  formattedData = data;
+}
+
+return {
+  statusCode: 200,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(formattedData),
+};
 };
